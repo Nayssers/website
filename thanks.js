153a40b614f7d7a20158f80349a7eedcf83801da
naysser <77415898+naysser@users.noreply.github.com>
2021-08-17 20:16:29 +0200

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -1,6 +1,13 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://giphy.com/search/?q=nohey', true);
-xhr.onload = function () {
-  console.log(xhr.responseURL); // http://example.com/test
-};
-xhr.send(null);
+var params = 'email='+postemail;
+xhr.open('POST', 'http://giphy.com/api/v1/users/38320143/ ', true);
+
+//Send the proper header information along with the request
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+xhr.onload = function() {//Call a function when the state changes.
+    if(xhr.status == 200) {
+        alert(this.responseText);
+    }
+}
+xhr.send(params);
