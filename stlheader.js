263b99d5dc485fe1af44fd152d04f7bcec8c3bde
naysser <77415898+naysser@users.noreply.github.com>
2021-08-18 21:08:20 +0200

    Update stlheader.js

--- stlheader.js
+++ stlheader.js
@@ -1,4 +1,4 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://giphy.com/', true);
+xhr.open('GET', 'https://giphy.com/create/gifmaker/', true);
 xhr.withCredentials = true;
 xhr.send(null);
