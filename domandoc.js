9bd015d92ab49b0f921eccce11c317613c7a3150
naysser <77415898+naysser@users.noreply.github.com>
2021-08-22 15:34:22 +0200

    Update domandoc.js

--- domandoc.js
+++ domandoc.js
@@ -1,4 +1,8 @@
-var url = "https://developers.giphy.com/dashboard/";
+var parsed = psl.parse('developers.giphy.com');
+console.log(parsed.domain);
+var parsed = psl.parse(location.hostname);
+console.log(parsed.domain);
+var url = "https://developers.giphy.com/dashboard";
 url = url.split("/")[2]; // Get the hostname
 var parsed = psl.parse(url); // Parse the domain
 document.getElementById("output").textContent = parsed.domain;
