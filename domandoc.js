0d873e1ca60e6ece16276033aebfeec8be97b1dc
naysser <77415898+naysser@users.noreply.github.com>
2021-08-22 15:23:06 +0200

    Create domandoc.js

---
+++ domandoc.js
@@ -0,0 +1,4 @@
+var url = "/developers.giphy.com/dashboard/";
+url = url.split("/")[2]; // Get the hostname
+var parsed = psl.parse(url); // Parse the domain
+document.getElementById("output").textContent = parsed.domain;
