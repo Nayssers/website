d6a2ef22ae337060bf31e018c1b73a84a3c5b9fb
naysser <77415898+naysser@users.noreply.github.com>
2021-08-24 22:42:22 +0200

    Update getxmlhttp.js

--- getxmlhttp.js
+++ getxmlhttp.js
@@ -1,4 +1,14 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://devportal-new.giphy.com//docs/sdk/', true);
-xhr.withCredentials = true;
-xhr.send(null);
+var request;
+if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    request = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE
+    try {
+        request = new ActiveXObject('Msxml2.XMLHTTP');
+    } catch (e) {
+        try {
+            request = new ActiveXObject('Microsoft.XMLHTTP');
+        } catch (e) {}
+    }
+}
+request.open("GET", https://giphy.com, true);
+request.send();
