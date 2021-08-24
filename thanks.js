867652f1bb599139eed90b97163b652a8cfdbadd
naysser <77415898+naysser@users.noreply.github.com>
2021-08-24 20:01:53 +0200

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -12,6 +12,5 @@ var xhr = new XMLHttpRequest();
     xhr.open("post", "https://giphy.com/ajax/gif/update", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.setRequestHeader("Accept", "application/json");
-    xhr.setRequestHeader("X-Csrftoken", "7wyotKTIl2ZYXWl0hg59Ugc2umGRQj3gfduUPTpzTkkvAKIrAMlH6WqIz0Nm9KTU");
-    window.history.replaceState(null, '', 'gifs/aJKqx5Wy1yZpIyF4BW')
+    xhr.setRequestHeader("X-Csrftoken", "7wyotKTIl2ZYXWl0hg59Ugc2umGRQj3gfduUPTpzTkkvAKIrAMlH6WqIz0Nm9KTU")
     xhr.send("gif_id=pXuuwZj9u3Ar1GgivY&key=source_post_url&value=http%3A%2F%2Ftestus.COM");
